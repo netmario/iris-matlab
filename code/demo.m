3ecc774aeb24a43d66a71aa11eec5734e3ec9949
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2017-01-03 22:50:32 +0800

    update demo.m

--- code/demo.m
+++ code/demo.m
@@ -1,8 +1,16 @@
-close all;
-[iris1,mask1] = iris('eye.jpg');
-[iris2,mask2] = iris('eye2.jpg');
+output_dir_root = '../results';
+output_dir1 = strcat(output_dir_root, '/image1');
+output_dir2 = strcat(output_dir_root, '/image2');
 
-%save('codes.mat','iris1','iris2','mask1','mask2')
-%load('codes.mat')
+rmdir(output_dir_root,'s');
+mkdir(output_dir_root);
+mkdir(output_dir1);
+mkdir(output_dir2);
 
-dist = smallest_distance(iris1,iris2,mask1,mask2)
+image1 = '../dataset/001/L/S1001L01.jpg';
+image2 = '../dataset/001/L/S1001L02.jpg';
+
+[iris1, mask1] = iris(image1, output_dir1);
+[iris2, mask2] = iris(image2, output_dir2);
+
+dist = smallest_distance(iris1, iris2, mask1, mask2)
